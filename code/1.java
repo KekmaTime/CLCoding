6cefedebfaa406fa005cf543d51ccf02f2d6f3c2
sachxd <ananthbiji@gmail.com>
2023-10-07 17:33:23 +0530

    sum of digits

---
+++ code/1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        int sum = calculateSumOfDigits(number);
+
+        System.out.println("Sum of digits: " + sum);
+    }
+
+    public static int calculateSumOfDigits(int number) {
+        int sum = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        return sum;
+    }
+}
